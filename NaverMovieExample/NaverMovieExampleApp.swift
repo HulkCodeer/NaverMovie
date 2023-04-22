158901ff21e361ceddc43c44929ac12d1ea7c4d0
Hulkcodeer <parkhj@soft-berry.com>
2023-04-22 21:03:21 +0900

    :construction: Make Project

---
+++ NaverMovieExample/NaverMovieExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverMovieExampleApp.swift
+//  NaverMovieExample
+//
+//  Created by hyunjin on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NaverMovieExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
